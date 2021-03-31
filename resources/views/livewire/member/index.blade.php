a6d1772b1c5f40a3d63e718d121ab5a49e63d420
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-03-31 17:32:44 +0800

    send

--- resources/views/livewire/member/index.blade.php
+++ resources/views/livewire/member/index.blade.php
@@ -46,7 +46,7 @@
                                 <td class="align-middle">{{ $row->member_phone }}</td>
                                 <td class="align-middle">{{ number_format($row->contract_price, 2) }}</td>
                                 <td class="align-middle">{{ $row->referral->referral_token }}</td>
-                                <td class="align-middle">{{ $row->member_password? "activated": "not activated" }}</td>
+                                <td class="align-middle">{{ $row->member_user? "activated": "not activated" }}</td>
                                 <td class="align-middle">{{ $row->created_at }}</td>
                                 <td class="with-btn-group align-middle text-right text-nowrap">
                                 @role('super-admin|user')
